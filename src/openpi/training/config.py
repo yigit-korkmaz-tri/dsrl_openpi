17903e4a43eeb9190142b941bd679e87b1ebfd20
yigit-korkmaz-tri <yigit.korkmaz@tri.global>
2026-08-14 14:45:42 -0400

    config change

--- src/openpi/training/config.py
+++ src/openpi/training/config.py
@@ -674,7 +674,7 @@ class TrainConfig:
     # How often (in steps) to save checkpoints.
     save_interval: int = 1000
     # If set, any existing checkpoints matching step % keep_period == 0 will not be deleted.
-    keep_period: int | None = 5000
+    keep_period: int | None = 2000
 
     # If true, will overwrite the checkpoint directory if it already exists.
     overwrite: bool = False
